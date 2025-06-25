5de7b1caa45192eb0e28c9d86cf87e7e5985358c
jimbo23000 <antonio.ramos23@yahoo.com>
2025-06-24 19:34:56 -0500

    Version 0.5

---
+++ delete-commands.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const discordToken = process.env.DISCORD_TOKEN;
+const applicationId = process.env.APPLICATION_ID;
+const serverId = process.env.SERVER_ID;
+
+const { REST, Routes } = require('discord.js');
+const rest = new REST().setToken(discordToken);
+
+const args = process.argv;
+
+if (args.length > 2) {
+    rest.delete(Routes.applicationGuildCommand(applicationId, serverId, args[2]))
+        .then(() => console.log('Successfully deleted guild command'))
+        .catch(console.error);
+    /*
+    // Code for global deletion
+    rest.delete(Routes.applicationCommand(applicationId, 'commandId'))
+        .then(() => console.log('Successfully deleted application command'))
+        .catch(console.error);
+    */
+} else {
+    rest.put(Routes.applicationGuildCommands(applicationId, serverId), { body: [] })
+        .then(() => console.log('Successfully deleted all guild commands.'))
+        .catch(console.error);
+    /*
+    // Code for global deletion
+    rest.put(Routes.applicationCommands(applicationId), { body: [] })
+        .then(() => console.log('Successfully deleted all application commands.'))
+        .catch(console.error);
+    */
+}
